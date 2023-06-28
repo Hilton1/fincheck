66abf759ae6232a799622c765a464dfc5e62aafe
Hilton1 <josehilton02@gmail.com>
2023-06-28 11:08:59 -0300

    Criando cadastro de usuário

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,9 +1,18 @@
+import { PrismaService } from './../database/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const user = await this.prismaService.user.create({
+      data: { name, email, password },
+    });
+
+    return user;
   }
 }
