fcce8f4d0a5ac4feb6cb72857f4f02e389a9012b
Hilton1 <josehilton02@gmail.com>
2023-06-30 10:18:11 -0300

    Criando DatabaseModule e refatorando código

--- api/src/modules/users/users.module.ts
+++ api/src/modules/users/users.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from 'src/shared/database/prisma.service';
-import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService],
 })
 export class UsersModule {}
