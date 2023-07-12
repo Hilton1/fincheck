ebcf71da089b2cf271578bead618ede1e7e5c882
Hilton1 <josehilton02@gmail.com>
2023-07-12 19:32:57 -0300

    validando credenciais do usuário

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthenticateDto } from './dto/authenticate.dto';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+import { compare } from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersRepo: UsersRepository) {}
+
+  async authenticate(authenticateDto: AuthenticateDto) {
+    const { email, password } = authenticateDto;
+
+    const user = await this.usersRepo.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials.');
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials.');
+    }
+
+    return { user };
+  }
+}
