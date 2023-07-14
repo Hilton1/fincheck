add4ef1d5dcc6f87346582a7d44974db76bf13cf
Hilton1 <josehilton02@gmail.com>
2023-07-14 01:46:02 -0300

    Atualizando autenticação

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,3 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
+import { UsersService } from './users.service';
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  me(@Request() request: any) {
+    console.log({ meuUserId: request.userId });
+
+    return this.usersService.getUserById('userId');
+  }
+}
