8ef35dffce32f674256a3e591e578f0b3e40cb5b
Hilton1 <josehilton02@gmail.com>
2023-07-18 01:02:46 -0300

    Refatorando configuração de rotas públicas

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 
+@IsPublic()
 @Controller('auth')
-@SetMetadata('IS_PUBLIC', true)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
