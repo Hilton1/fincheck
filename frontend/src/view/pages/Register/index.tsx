acd7eb51848f9b8894755c6c92cf380af60b942f
Hilton1 <josehilton02@gmail.com>
2023-09-06 01:00:04 -0300

    validating register form

--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,45 +1,54 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import { useRegisterController } from "./useRegisterController";
 
 export function Register() {
+  const { handleSubmit, errors, register } = useRegisterController();
+
   return(
     <>
-      <div className="flex flex-col items-center gap-4 text-center">
-      <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
-        Crie sua conta
-      </h1>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Crie sua conta
+        </h1>
 
-      <p className="space-x-2">
-        <span className="text-gray-700 tracking-[-0.5px]">
-          Já possui uma conta?
-        </span>
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
 
-        <Link
-          to='/login'
-          className="tracking-[-0.5px] font-medium text-teal-900"
-        >
-          Fazer login
-        </Link>
-      </p>
+          <Link
+            to='/login'
+            className="tracking-[-0.5px] font-medium text-teal-900"
+          >
+            Fazer login
+          </Link>
+        </p>
+      </header>
 
-      </div>
-      <form className="mt-[60px] flex flex-col gap-4">
+      <form
+        onSubmit={handleSubmit}
+        className="mt-[60px] flex flex-col gap-4"
+      >
         <Input
           placeholder="Nome"
-          name="name"
+          error={errors.name?.message}
+          {...register('name')}
         />
 
         <Input
           type="email"
           placeholder="E-mail"
-          name="email"
+          error={errors.email?.message}
+          {...register('email')}
         />
 
         <Input
           type="password"
           placeholder="Senha"
-          name="password"
+          error={errors.password?.message}
+          {...register('password')}
         />
 
         <Button type="submit" className="mt-2">
---
+++ frontend/src/view/pages/Register/useRegisterController.ts
@@ -0,0 +1,35 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().
+    nonempty('Nome é obrigatório'),
+
+  email: z.string().
+    nonempty('E-mail é obrigatório').
+    email('Informe um e-mail válido'),
+
+  password: z.string().
+    nonempty('Senha é obrigatória').
+    min(8, 'A senha deve conter, pelo menos, 8 dígitos'),
+})
+
+type FormData = z.infer<typeof schema>
+
+export function useRegisterController() {
+  const {
+    register,
+    handleSubmit: hookFormHandleSubmit,
+    formState: { errors },
+  } =  useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+
+  const handleSubmit = hookFormHandleSubmit((data) => {
+    console.log('Chama a API com:', data)
+  });
+
+  return { handleSubmit, register, errors }
+}
