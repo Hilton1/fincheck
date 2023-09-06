65b0fdf6a460bffba36db887ce4b66a6c41ff34c
Hilton1 <josehilton02@gmail.com>
2023-09-05 23:58:51 -0300

    addign react-hook-form and refactoring code

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -1,40 +1,45 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import { useLoginController } from "./useLoginController";
 
 export function Login() {
+  const { handleSubmit, register } = useLoginController();
   return(
     <>
-      <div className="flex flex-col items-center gap-4 text-center">
-      <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
-        Entre em sua conta
-      </h1>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Entre em sua conta
+        </h1>
 
-      <p className="space-x-2">
-        <span className="text-gray-700 tracking-[-0.5px]">
-          Novo por aqui?
-        </span>
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Novo por aqui?
+          </span>
 
-        <Link
-          to='/register'
-          className="tracking-[-0.5px] font-medium text-teal-900"
-        >
-          Crie uma conta
-        </Link>
-      </p>
+          <Link
+            to='/register'
+            className="tracking-[-0.5px] font-medium text-teal-900"
+          >
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
 
-      </div>
-      <form className="mt-[60px] flex flex-col gap-4">
+      <form
+        onSubmit={handleSubmit}
+        className="mt-[60px] flex flex-col gap-4"
+      >
         <Input
           type="email"
           placeholder="E-mail"
-          name="email"
+          {...register('email')}
         />
 
         <Input
           type="password"
           placeholder="Senha"
-          name="password"
+          {...register('password')}
         />
 
         <Button type="submit" className="mt-2">
