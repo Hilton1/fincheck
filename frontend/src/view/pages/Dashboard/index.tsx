61078d1b1b734ce8f08e8dd396041665f4b71454
Hilton1 <josehilton02@gmail.com>
2023-09-15 14:53:22 -0300

    adding header in dashboard page

--- frontend/src/view/pages/Dashboard/index.tsx
+++ frontend/src/view/pages/Dashboard/index.tsx
@@ -1,17 +1,16 @@
-import { useAuth } from "../../../app/hooks/useAuth";
-import { Button } from "../../components/Button";
+import { Logo } from "../../components/Logo";
+import { UserMenu } from "../../components/UserMenu";
 
 export function Dashboard() {
-  const { signout } = useAuth();
-
-
   return(
-    <div>
-      <h1>Dashboard Page</h1>
-
-      <Button onClick={signout}>
-        Sair
-      </Button>
+    <div className="h-full w-full px-8 pb-8 pt-6 flex flex-col gap-4">
+      <header className="h-12 flex items-center justify-between">
+        <Logo className="h-6 text-teal-900" />
+        <UserMenu />
+      </header>
+      <main>
+        Content
+      </main>
     </div>
   );
 }
