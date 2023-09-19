975a605e51c09b261f8fd609e4ed88281af77609
Hilton1 <josehilton02@gmail.com>
2023-09-18 21:53:11 -0300

    adding BankAccountTypeIcon

---
+++ frontend/src/view/components/icons/BankAccountTypeIcon/index.tsx
@@ -0,0 +1,11 @@
+import { iconsMap } from "./inconsMap";
+
+interface BankAccountTypeIconProps {
+  type: keyof typeof iconsMap;
+}
+
+export function BankAccountTypeIcon({ type }: BankAccountTypeIconProps) {
+  const Icon = iconsMap[type];
+
+  return <Icon />;
+}
