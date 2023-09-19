e6e3a1f5d91d964581ea42ec0f13d05f0a98746c
Hilton1 <josehilton02@gmail.com>
2023-09-18 21:02:26 -0300

    adding disabled button

--- frontend/src/view/pages/Dashboard/components/Accounts.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts.tsx
@@ -1,7 +1,46 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import { EyeIcon } from "../../../components/icons/EyeIcon";
+
 export function Accounts() {
   return (
-    <div className="bg-teal-900 rounded-2xl h-full w-full px-4 py-8 md:p-10">
-      Accounts
+    <div className="bg-teal-900 rounded-2xl h-full w-full px-4 py-8 md:p-10 flex flex-col">
+      <div>
+        <span className="tracking-[-0.5px] text-white block">Saldo total</span>
+
+        <div className="flex items-center gap-2">
+          <strong className="text-2xl tracking-[-1px] text-white">
+            R$: 1000,00
+          </strong>
+          <button className="w-8 h-8 flex items-center justify-center">
+            <EyeIcon open/>
+          </button>
+        </div>
+      </div>
+
+      <div className="flex-1 flex flex-col justify-end">
+        <div className="flex items-center justify-between">
+          <strong className="text-white tracking-[-1px] text-lg">
+            Minhas contas
+          </strong>
+
+          <div>
+            <button
+             className="py-3 pl-2.5 pr-3.5 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40"
+             disabled
+            >
+              <ChevronLeftIcon className="text-white h-6 w-6" />
+            </button>
+
+            <button
+             className="py-3 pl-2.5 pr-3.5 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40"
+            >
+              <ChevronRightIcon className="text-white h-6 w-6" />
+            </button>
+          </div>
+        </div>
+
+        <div>contas</div>
+      </div>
     </div>
   );
 }
