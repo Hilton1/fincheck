f311358b0041ab39876350ae53b12fcf0feded4f
Hilton1 <josehilton02@gmail.com>
2023-10-03 10:26:42 -0300

    adding DropdowMenu in Transaction

--- frontend/src/view/components/DropdownMenu.tsx
+++ frontend/src/view/components/DropdownMenu.tsx
@@ -27,7 +27,7 @@ function DropdownMenuContent({ children, className }: DropdownMenuContentProps) 
     <RdxDropdownMenu.Portal>
       <RdxDropdownMenu.Content
         className={cn(
-          'rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] data-[side=bottom]:animate-slide-up-and-fade',
+          'rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] data-[side=bottom]:animate-slide-up-and-fade z-50',
           className
         )}
       >
@@ -48,7 +48,7 @@ function DropdownMenuItem({ children, className, onSelect }: DropdownMenuItemPro
     <RdxDropdownMenu.Item
       onSelect={onSelect}
       className={cn(
-        'min-h-[48px] outline-none flex items-center p-4 text-sm text-gray-800 data-[highlighted]:bg-gray-50 rounded-2xl transition-colors cursor-pointer',
+        'min-h-[40px] outline-none flex items-center px-4 py-2 text-sm text-gray-800 data-[highlighted]:bg-gray-50 rounded-2xl transition-colors cursor-pointer',
         className,
       )}
     >
